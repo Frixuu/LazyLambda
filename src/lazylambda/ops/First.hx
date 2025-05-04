017357125fa0ca39d0adbf9045c757f2d000a729
Frixuu <kontakt@lukasz.xyz>
2025-05-05 01:58:34 +0200

    Add more `first` variants

--- src/lazylambda/ops/First.hx
+++ src/lazylambda/ops/First.hx
@@ -1,10 +1,90 @@
 // SPDX-License-Identifier: MIT
 package lazylambda.ops;
 
+import haxe.ds.Option;
 import lazylambda.NoItemsException;
 
 final class First {
 
+    /**
+        Moves the first available item out from the source iterator, if it has one.
+        @param src The source iterator.
+        @return The first item of source, or `None` if the source iterator was empty.
+    **/
+    public static extern inline overload function first<T>(src: Iterator<T>): Option<T> {
+        return if (!src.hasNext()) {
+            None;
+        } else {
+            Some(src.next());
+        };
+    }
+    
+    /**
+        Drains the source iterator until the first matching item, if it has one.
+        @param src The source iterator.
+        @param predicate The condition to match.
+        @return The first matching item of source, or `None` if there was none.
+    **/
+    public static extern inline overload function first<T>(
+        src: Iterator<T>,
+        predicate: (item: T) -> Bool
+    ): Option<T> {
+    
+        var item: Option<T> = None;
+        while (src.hasNext()) {
+            final candidate = src.next();
+            if (predicate(candidate)) {
+                item = Some(candidate);
+                break;
+            }
+        }
+        
+        return item;
+    }
+    
+    /**
+        Moves the first available item out from the source iterator, if it has one.
+        @param src The source iterator.
+        @return The first item of source, or `null` if the source iterator was empty.
+    **/
+    public static extern inline overload function firstOrNull<T>(src: Iterator<T>): Null<T> {
+    
+        if (!src.hasNext()) {
+            return null;
+        }
+        
+        return src.next();
+    }
+    
+    /**
+        Drains the source iterator until the first matching item, if it has one.
+        @param src The source iterator.
+        @param predicate The condition to match.
+        @return The first matching item of source, or `null` if there was none.
+    **/
+    public static extern inline overload function firstOrNull<T>(
+        src: Iterator<T>,
+        predicate: (item: T) -> Bool
+    ): Null<T> {
+    
+        var item: Null<T> = null;
+        while (src.hasNext()) {
+            final candidate = src.next();
+            if (predicate(candidate)) {
+                item = candidate;
+                break;
+            }
+        }
+        
+        return item;
+    }
+    
+    /**
+        Moves the first available item out from the source iterator, if it has one.
+        @param src The source iterator.
+        @return The first item of source.
+        @throws NoItemsException The source iterator was empty.
+    **/
     public static extern inline overload function firstOrThrow<T>(src: Iterator<T>): T {
     
         if (!src.hasNext()) {
@@ -14,25 +94,22 @@ final class First {
         return src.next();
     }
     
+    /**
+        Drains the source iterator until the first matching item, if it has one.
+        @param src The source iterator.
+        @param predicate The condition to match.
+        @return The first matching item of source.
+        @throws NoItemsException The source iterator had no matching items.
+    **/
     public static extern inline overload function firstOrThrow<T>(
         src: Iterator<T>,
-        predicate: (T) -> Bool
+        predicate: (item: T) -> Bool
     ): T {
-        return firstOrThrowPredicateImpl(src, predicate);
-    }
-    
-    private static function firstOrThrowPredicateImpl<T>(
-        src: Iterator<T>,
-        predicate: (T) -> Bool
-    ): T {
-    
-        while (src.hasNext()) {
-            final item = src.next();
-            if (predicate(item)) {
-                return item;
-            }
-        }
-        
-        throw new NoItemsException();
+        return switch (first(src, predicate)) {
+            case Some(item):
+                item;
+            case None:
+                throw new NoItemsException();
+        };
     }
 }
--- tests/src/FirstTest.hx
+++ tests/src/FirstTest.hx
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 package;
 
+import haxe.Exception;
+import haxe.ds.Option;
 import utest.Assert;
 import utest.Test;
 
@@ -8,12 +10,36 @@ using lazylambda.Prelude;
 
 class FirstTest extends Test {
 
-    public function test__Getting_the_first_element_does_not_drain_source_fully() {
+    public function test__First_works() {
+        final src = (0...4);
+        Assert.same(Some(0), src.first());
+        Assert.isTrue(src.hasNext());
+        Assert.equals(1, src.next());
+        Assert.same(Some(3), src.first(i -> i > 2));
+        Assert.isFalse(src.hasNext());
+        Assert.equals(None, src.first());
+        Assert.equals(None, src.first(i -> i > 2));
+    }
+    
+    public function test__First_nullable_works() {
+        final src = (0...4);
+        Assert.equals(0, src.firstOrNull());
+        Assert.isTrue(src.hasNext());
+        Assert.equals(1, src.next());
+        Assert.equals(3, src.firstOrNull(i -> i > 2));
+        Assert.isFalse(src.hasNext());
+        Assert.equals(null, src.firstOrNull());
+        Assert.equals(null, src.firstOrNull(i -> i > 2));
+    }
+    
+    public function test__First_throwable_works() {
         final src = (0...4);
         Assert.equals(0, src.firstOrThrow());
         Assert.isTrue(src.hasNext());
         Assert.equals(1, src.next());
         Assert.equals(3, src.firstOrThrow(i -> i > 2));
         Assert.isFalse(src.hasNext());
+        Assert.raises(() -> src.firstOrThrow(), Exception);
+        Assert.raises(() -> src.firstOrThrow(i -> i > 2), Exception);
     }
 }
