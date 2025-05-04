3c9e79b538210269cb4bbc59bd022b7b588f9fd6
Frixuu <kontakt@lukasz.xyz>
2025-05-05 00:05:41 +0200

    Rename `toMap`

--- src/lazylambda/ops/Collect.hx
+++ src/lazylambda/ops/Collect.hx
@@ -44,7 +44,9 @@ final class Collect {
         @return A new map.
     **/
     @:generic
-    public static inline function toMap<K, V>(src: KeyValueIterator<K, V>): haxe.ds.Map<K, V> {
+    public static extern inline overload function toMap<T: KeyValueIterator<K, V>, K, V>(
+        src: T
+    ): haxe.ds.Map<K, V> {
         return [for (key => value in src) key => value];
     }
     
@@ -56,7 +58,7 @@ final class Collect {
         @return A new map.
     **/
     @:generic
-    public static inline function toMapBy<T, K, V>(
+    public static extern inline overload function toMap<T, K, V>(
         src: Iterator<T>,
         keySelector: (T) -> K,
         valueSelector: (T) -> V
