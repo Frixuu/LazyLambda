367641ed543c24b6fbe69c2e2e584a254fd6919c
Frixuu <kontakt@lukasz.xyz>
2025-05-05 00:05:41 +0200

    Rename `toMapBy` back to a `toMap` overload

--- tests/src/CollectTest.hx
+++ tests/src/CollectTest.hx
@@ -29,7 +29,7 @@ class CollectTest extends Test {
     }
     
     public function test__Collecting_an_IntIterator_to_a_map_works() {
-        final m = (0...3).toMapBy(i -> Std.string(i), i -> i);
+        final m = (0...3).toMap(i -> Std.string(i), i -> i);
         Assert.same(["0" => 0, "1" => 1, "2" => 2], m);
     }
     
@@ -41,7 +41,7 @@ class CollectTest extends Test {
     
     public function test__Collecting_a_map_iterator_to_a_map_with_selectors_works() {
         final src = ["0" => 0, "1" => 1, "2" => 2];
-        final dst = src.keyValueIterator().toMapBy(pair -> pair.key, pair -> pair.value + 1);
+        final dst = src.keyValueIterator().toMap(pair -> pair.key, pair -> pair.value + 1);
         Assert.same(["0" => 1, "1" => 2, "2" => 3], dst);
     }
 }
