1c7630ede2f64799f60808cb8a28b62f12aca412
Frixuu <kontakt@lukasz.xyz>
2025-05-08 02:21:36 +0200

    Update ReduceTest.hx

--- tests/src/ReduceTest.hx
+++ tests/src/ReduceTest.hx
@@ -12,14 +12,14 @@ class ReduceTest extends Test {
 
     public function test__Strings_can_be_reduced() {
         final src: Array<String> = ["he", "ll", "o ", "wor", "ld!"];
-        Assert.same(Some("hello world!"), src.iterator().reduce((a, s) -> a + s));
+        Assert.same(Some("hello world!"), (src.iterator().reduce((a, s) -> a + s): Null<Dynamic>));
         Assert.equals("hello world!", src.iterator().reduceOrNull((a, s) -> a + s));
         Assert.equals("hello world!", src.iterator().reduceOrThrow((a, s) -> a + s));
     }
     
     public function test__Empty_iterator_is_treated_appropriately() {
         final src: Array<String> = [];
-        Assert.equals(None, src.iterator().reduce((a, s) -> a + s));
+        Assert.equals(None, (src.iterator().reduce((a, s) -> a + s): Null<Dynamic>));
         Assert.equals(null, src.iterator().reduceOrNull((a, s) -> a + s));
         Assert.raises(() -> src.iterator().reduceOrThrow((a, s) -> a + s), Exception);
     }
