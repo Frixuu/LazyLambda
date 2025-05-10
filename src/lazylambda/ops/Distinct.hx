203c05cefc881952f1f60250dcd4a30df90e3337
Frixuu <kontakt@lukasz.xyz>
2025-05-11 01:05:38 +0200

    Add `.distinctByIdentity()`

--- src/lazylambda/ops/Distinct.hx
+++ src/lazylambda/ops/Distinct.hx
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 package lazylambda.ops;
 
+import haxe.ds.IntMap;
+import haxe.ds.ObjectMap;
+import haxe.ds.StringMap;
+import lazylambda.ds.Comparable;
 import lazylambda.ds.RbTreeSet;
 import lazylambda.iterators.IIterator;
 import lazylambda.iterators.IteratorFiltering;
@@ -13,10 +17,67 @@ final class Distinct {
         @return A new iterator.
     **/
     @:generic
-    public static extern inline overload function distinct<S: Iterator<Int>>(
-        src: S
-    ): IIterator<Int> {
-        final treeSet = new RbTreeSet<Int>((a, b) -> a - b);
+    public static extern inline overload function distinct<S_Comp: Iterator<T>, T: Comparable<T>>(
+        src: S_Comp
+    ): IIterator<T> {
+        final treeSet = new RbTreeSet<T>((a, b) -> a.compareTo(b));
         return new IteratorFiltering(src, item -> treeSet.insert(item));
+    }
+    
+    /**
+        Returns only unique items from the source iterator.
+        @param src The source iterator.
+        @return A new iterator.
+    **/
+    @:generic
+    public static extern inline overload function distinct<S_Int: Iterator<Int>>(
+        src: S_Int
+    ): IIterator<Int> {
+        final set: IntMap<Bool> = new IntMap();
+        return new IteratorFiltering(src, item -> {
+            final wasAbsent = !set.exists(item);
+            if (wasAbsent) {
+                set.set(item, true);
+            }
+            return wasAbsent;
+        });
+    }
+    
+    /**
+        Returns only unique items from the source iterator.
+        @param src The source iterator.
+        @return A new iterator.
+    **/
+    @:generic
+    public static extern inline overload function distinct<S_String: Iterator<String>>(
+        src: S_String
+    ): IIterator<String> {
+        final set: StringMap<Bool> = new StringMap();
+        return new IteratorFiltering(src, item -> {
+            final wasAbsent = !set.exists(item);
+            if (wasAbsent) {
+                set.set(item, true);
+            }
+            return wasAbsent;
+        });
+    }
+    
+    /**
+        Returns only unique references from the source iterator.
+        @param src The source iterator.
+        @return A new iterator.
+    **/
+    @:generic
+    public static extern inline overload function distinctByIdentity<S_Ref: Iterator<T>, T: {}>(
+        src: S_Ref
+    ): IIterator<T> {
+        final set: ObjectMap<T, Bool> = new ObjectMap();
+        return new IteratorFiltering(src, item -> {
+            final wasAbsent = !set.exists(item);
+            if (wasAbsent) {
+                set.set(item, true);
+            }
+            return wasAbsent;
+        });
     }
 }
--- tests/src/DistinctTest.hx
+++ tests/src/DistinctTest.hx
@@ -28,4 +28,58 @@ class DistinctTest extends Test {
         final iter = array.iterator().distinct();
         Assert.same(array, (iter.toArray(): Null<Dynamic>));
     }
+    
+    public function test__Distinct_works_with_strings() {
+    
+        final array = ["ab", "ac", "ab", "account", "12", "null", "undefined", "undefined"];
+        final iter = array.iterator().distinct();
+        Assert.equals("ab", iter.next());
+        Assert.equals("ac", iter.next());
+        Assert.equals("account", iter.next());
+        Assert.equals("12", iter.next());
+        Assert.equals("null", iter.next());
+        Assert.equals("undefined", iter.next());
+        Assert.isFalse(iter.hasNext());
+    }
+    
+    public function test__Distinct_works_with_comparables() {
+    
+        final array = [new Foo(1), new Foo(2), new Foo(1), new Foo(3), new Foo(2)];
+        final iter = array.iterator().distinct();
+        Assert.equals(1, iter.next().bar);
+        Assert.equals(2, iter.next().bar);
+        Assert.equals(3, iter.next().bar);
+        Assert.isFalse(iter.hasNext());
+    }
+    
+    public function test__Distinct_by_identity_works() {
+    
+        final one = new Foo(1);
+        final two = new Foo(2);
+        final three = new Foo(3);
+        final four = new Foo(4);
+        final array = [new Foo(1), new Foo(1), two, two, four, new Foo(4), three, one, one];
+        final iter = array.iterator().distinctByIdentity();
+        Assert.equals(1, iter.next().bar);
+        Assert.equals(1, iter.next().bar);
+        Assert.equals(2, iter.next().bar);
+        Assert.equals(4, iter.next().bar);
+        Assert.equals(4, iter.next().bar);
+        Assert.equals(3, iter.next().bar);
+        Assert.equals(1, iter.next().bar);
+        Assert.isFalse(iter.hasNext());
+    }
+}
+
+class Foo {
+
+    public final bar: Int;
+    
+    public function new(bar: Int) {
+        this.bar = bar;
+    }
+    
+    public function compareTo(other: Foo): Int {
+        return this.bar - other.bar;
+    }
 }
